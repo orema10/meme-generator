3cafe87d9dbd3689e859bcd0e071b9a6e19c51ff
Tamir <tamirdalal10@gmil.com>
2018-03-20 17:42:41 +0200

    updated css name and model started

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('Hello');
+var nextIdCount = 1;
+
+var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}];
+
+
+
+function createImg (nextId,url,keywords) {
+return {
+id: nextId,
+url: url,
+keywords: keywords,
+}
+}
